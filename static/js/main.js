d425c774f64a8410faae3959b71c637e37d6782a
p0lygun <solankivibhakar82@gmail.com>
2022-09-05 18:31:15 +0530

    feat: use ranger api to dynamically set online status

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,32 @@
+window.addEventListener('load', schedule_set_online_presence)
+
+async function schedule_set_online_presence(){
+    await fetch("https://ranger.stilllearning.tech/user/?id=338947895665360898&guild_id=870246147455877181").then(
+        resp => resp.json().then(json =>set_online_presence(json)))
+
+    setTimeout(schedule_set_online_presence,60000); // call itself every 1 min
+}
+
+function set_online_presence(json){
+    if(json['status']){
+        const status_element = document.getElementById("online-status"),
+            status_dot = document.getElementById("online-status-dot");
+        if(json['message'] === "online"){
+            status_element.textContent = "Online"
+            if(status_dot.classList.contains("offline")){
+                status_dot.classList.remove("offline");
+            }
+            status_dot.classList.add("online");
+        } else {
+            status_element.textContent = "Offline"
+            if(status_dot.classList.contains("online")){
+                status_dot.classList.remove("online");
+            }
+            status_dot.classList.add("offline");
+        }
+    }
+}
+
 function checkCommandInput(event){
     if (event.key === "Enter") {
         const command = document.getElementById("command-input").value;
